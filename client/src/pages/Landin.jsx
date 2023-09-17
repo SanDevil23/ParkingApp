68bd8ec421be520604daf52baa5bb7776534a744
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2023-09-17 09:04:49 +0530

    login page completed

--- client/src/pages/Landin.jsx
+++ client/src/pages/Landin.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./templates/landing.css";
 import { useNavigate } from "react-router-dom";
+import { Player } from "@lottiefiles/react-lottie-player";
+import animationEffect from "../components/lottie/animation_lmk5pwtx.json";
 
 export default function Landing() {
   const navigator = useNavigate();
@@ -13,9 +15,16 @@ export default function Landing() {
 
       <div className="content">
         <div className="logo">
-          <img src="https://cdn.dribbble.com/users/1287580/screenshots/5410442/dribbble_2.gif"></img>
+        <Player
+          src={animationEffect}
+          className="player"
+          loop
+          autoplay
+          speed={"2"}
+          style={{ height: "600px", width: "600px" }}
+        />
         </div>
-
+       
         <div className="title">
           <p id="appNm">ParkMe</p>
           <p>Only solution for parking in India</p>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./templates/login.css";
 import { Player } from "@lottiefiles/react-lottie-player";
-import animationEffect from "../components/animation_lmip8qge.json";
+import animationEffect from "../components/lottie/animation_lmip8qge.json";
+import { FcGoogle,FcAndroidOs} from 'react-icons/fc';
+import { MdKeyboardBackspace } from 'react-icons/md';
+
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigator = useNavigate();
   return (
     <div className="wrapper">
+
       <div>
+      <button className="backBtn" onClick={()=>navigator(-1)}><MdKeyboardBackspace className="bk-icon"/></button>
+      {/* <button className="backBtn" onClick={()=>navigator('/')}>Home</button> */}
+             
         <Player
           src={animationEffect}
           className="player"
@@ -15,11 +24,14 @@ export default function Login() {
           speed={"2"}
           style={{ height: "600px", width: "600px" }}
         />
+        
         <div className="loginBtnDiv">
-          <button className="googleBtn">Login with google</button>
-          <button className="phoneBtn">Login with Phone Number</button>
+          <button className="googleBtn"><FcGoogle className="google"/> Login With Google</button>
+          <button className="phoneBtn"><FcAndroidOs className="phone"/>Login With Phone</button>
         </div>
       </div>
     </div>
   );
 }
+
+
